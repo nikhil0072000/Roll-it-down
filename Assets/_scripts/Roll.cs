71ba9f49718772e18cc19b772ffa4b1f592104ff
Nikhil144bc <51047806+nikhil0072000@users.noreply.github.com>
2022-12-18 21:32:08 +0530

    dupicate

--- Assets/_scripts/Roll.cs
+++ Assets/_scripts/Roll.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class Roll : MonoBehaviour
 {
+    public GameObject obj;
     public Material shader;
+    
     public float unroll;
     public float roll;
     // Start is called before the first frame update
     void Start()
     {
-        shader.SetFloat("_RollCenterPosX", unroll / roll);
+        
+       
+        shader.SetFloat("_RollCenterPosX", unroll);
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         if (Input.GetMouseButton(0))
         {
             unroll +=5;
             
-                shader.SetFloat("_RollCenterPosX", unroll / roll);
+                shader.SetFloat("_RollCenterPosX", unroll);
             if (unroll > 440f)
         
            unroll = 440f;
